7b0731391800594e41cb8f37f461d56272856347
Sam Knott <SKnott@smchull.org>
2022-03-17 13:41:42 +0000

    //Removed the comment

--- Jokemon/Game1.cs
+++ Jokemon/Game1.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Jokemon
-{//##
+{
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
